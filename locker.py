559045211c846ea8bccd029922091b806f4ce763
Gui Talarico <gtalarico@gmail.com>
2016-02-27 15:59:46 -0500

    Stack Overflow suggestions

--- locker.py
+++ locker.py
@@ -1,69 +1,74 @@
 class Locker(object):
 
     def __init__(self, capacity):
-        self.chambers = {}
-
         print 'Locker Initialized.'
-        for n, chamber in enumerate(range(0, capacity)):
-            self.chambers[n] = Chamber(n)
+        self.chambers = []
+        self.chambers = [Chamber(n) for n in range(capacity)]
         print 'Locker Created: ', self
 
-    def find_chamber_by_pin(self, pin):
-        for chamber in self.chambers.values():
-            if pin == getattr(chamber.user, 'pin', None):
-                return chamber
-        return None
+    @property
+    def available_chambers(self):
+        return [chamber for chamber in self.chambers if chamber.available]
+
+    @property
+    def occupied_chambers(self):
+        return [chamber for chamber in self.chambers if chamber.occupied]
 
     def find_available_chamber(self):
-        for chamber in self.chambers.values():
-            if chamber.occupied is False:
+        try:
+            return self.available_chambers[0]
+        except IndexError:
+            return None
+
+    def find_chamber_by_pin(self, pin):
+        for chamber in self.occupied_chambers:
+            if pin == chamber.user.pin:
                 return chamber
         return None
 
     def count_available(self):
-        counter = 0
-        for chamber in self.chambers.values():
-            if chamber.occupied is False:
-                counter += 1
-        return counter
+        return len(self.available_chambers)
 
     def __len__(self):
-        return len(self.chambers.keys())
+        return len(self.chambers)
 
     def __repr__(self):
-        return '<LOCKER: {} of {} Available>:'.format(self.count_available(), len(self))
+        return '<LOCKER: {} of {} Available>:'.format(self.count_available(),
+                                                      len(self))
 
 
 class Chamber(object):
 
     def __init__(self, uid):
         self.uid = uid
-        self.occupied = False
         self.user = None
         print 'Chamber Initialized: ', self
 
+    @property
+    def occupied(self):
+        return bool(self.user)
+
+    @property
+    def available(self):
+        return not self.occupied
+
     def reserve(self, user):
-        self.occupied = True
         self.user = user
         print 'Chamber Reserved: ', self
 
     def release(self):
-        self.occupied = False
-        user = self.user
-        del user
         self.user = None
-
         print 'Chamber Released: ', self
 
     def __repr__(self):
         return '<CHAMBER {}:{}>:{}'.format(self.uid, self.occupied,
-                                             self.user)
+                                           self.user)
+
 
 class User(object):
 
     def __init__(self, pin):
         self.pin = pin
-
         print 'User Created: ', self
 
     def __repr__(self):
@@ -74,18 +79,14 @@ locker = Locker(4)
 while True:
 
     pin = raw_input('Locker Idle. Enter Unique PIN:')
-
     chamber = locker.find_chamber_by_pin(pin)
 
     if chamber:
         chamber.release()
-
     else:
         chamber = locker.find_available_chamber()
         if chamber:
-            user = User(pin)
-            chamber.reserve(user)
+            chamber.reserve(User(pin))
         else:
             print 'Chambers Full. Wait for relase.'
-
     print 'New Status: ', locker
